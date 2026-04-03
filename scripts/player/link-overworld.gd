0ea0f33b327a3b0150ea9ff20040e5d23e01abd1
Aya <Sherbnya@gmail.com>
2026-04-03 18:16:13 +0100

    Create basic project and add overworld movement

---
+++ scripts/player/link-overworld.gd
@@ -0,0 +1,159 @@
+extends Node2D
+
+@export var tilemap: TileMapLayer
+@export var move_speed: float = 70.0
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_moving: bool = false
+var target_position: Vector2
+var current_direction: Vector2i = Vector2i.ZERO
+var buffered_direction: Vector2i = Vector2i.ZERO
+var facing_direction: Vector2i = Vector2i.DOWN
+
+# Stores currently held directions in press order.
+# Last item = most recently pressed, which wins.
+var held_directions: Array[Vector2i] = []
+
+func _ready() -> void:
+	if tilemap == null:
+		push_error("tilemap is not assigned in the Inspector.")
+		set_process(false)
+		return
+
+	position = snap_to_tile_center(position)
+	target_position = position
+
+	update_animation()
+
+func _process(delta: float) -> void:
+	update_input_buffer()
+
+	if is_moving:
+		move_toward_target(delta)
+	else:
+		try_start_move()
+
+	update_animation()
+
+func update_input_buffer() -> void:
+	handle_direction_input("move_left", Vector2i.LEFT)
+	handle_direction_input("move_right", Vector2i.RIGHT)
+	handle_direction_input("move_up", Vector2i.UP)
+	handle_direction_input("move_down", Vector2i.DOWN)
+
+	# Remove any stale entries just in case
+	for i in range(held_directions.size() - 1, -1, -1):
+		if not is_direction_still_held(held_directions[i]):
+			held_directions.remove_at(i)
+
+	if held_directions.size() > 0:
+		buffered_direction = held_directions[held_directions.size() - 1]
+	else:
+		buffered_direction = Vector2i.ZERO
+
+func handle_direction_input(action: String, dir: Vector2i) -> void:
+	if Input.is_action_just_pressed(action):
+		held_directions.erase(dir)
+		held_directions.append(dir)
+
+	if Input.is_action_just_released(action):
+		held_directions.erase(dir)
+
+func is_direction_still_held(dir: Vector2i) -> bool:
+	match dir:
+		Vector2i.LEFT:
+			return Input.is_action_pressed("move_left")
+		Vector2i.RIGHT:
+			return Input.is_action_pressed("move_right")
+		Vector2i.UP:
+			return Input.is_action_pressed("move_up")
+		Vector2i.DOWN:
+			return Input.is_action_pressed("move_down")
+		_:
+			return false
+
+func try_start_move() -> void:
+	if buffered_direction == Vector2i.ZERO:
+		current_direction = Vector2i.ZERO
+		return
+
+	var current_cell := world_to_cell(position)
+	var next_cell := current_cell + buffered_direction
+
+	if is_cell_walkable(next_cell):
+		current_direction = buffered_direction
+		facing_direction = current_direction
+		target_position = cell_to_world(next_cell)
+		is_moving = true
+	else:
+		# Still face the pressed direction even if blocked (feels nicer)
+		facing_direction = buffered_direction
+
+func move_toward_target(delta: float) -> void:
+	position = position.move_toward(target_position, move_speed * delta)
+
+	if position.distance_to(target_position) < 0.01:
+		position = target_position
+		is_moving = false
+		try_continue_move()
+
+func try_continue_move() -> void:
+	if buffered_direction == Vector2i.ZERO:
+		current_direction = Vector2i.ZERO
+		return
+
+	var current_cell := world_to_cell(position)
+	var next_cell := current_cell + buffered_direction
+
+	if is_cell_walkable(next_cell):
+		current_direction = buffered_direction
+		facing_direction = current_direction
+		target_position = cell_to_world(next_cell)
+		is_moving = true
+	else:
+		current_direction = Vector2i.ZERO
+		facing_direction = buffered_direction
+
+func update_animation() -> void:
+	var anim_name := ""
+
+	if is_moving:
+		anim_name = "walk_" + direction_to_string(facing_direction)
+	else:
+		anim_name = "idle_" + direction_to_string(facing_direction)
+
+	if animation_player.current_animation != anim_name or not animation_player.is_playing():
+		animation_player.play(anim_name)
+
+func direction_to_string(dir: Vector2i) -> String:
+	match dir:
+		Vector2i.LEFT:
+			return "left"
+		Vector2i.RIGHT:
+			return "right"
+		Vector2i.UP:
+			return "up"
+		_:
+			return "down"
+
+func is_cell_walkable(cell: Vector2i) -> bool:
+	var tile_data := tilemap.get_cell_tile_data(cell)
+
+	if tile_data == null:
+		return false
+
+	var walkable = tile_data.get_custom_data("walkable")
+	return walkable == true
+
+func world_to_cell(world_pos: Vector2) -> Vector2i:
+	var local_pos := tilemap.to_local(world_pos)
+	return tilemap.local_to_map(local_pos)
+
+func cell_to_world(cell: Vector2i) -> Vector2:
+	var local_pos := tilemap.map_to_local(cell)
+	return tilemap.to_global(local_pos)
+
+func snap_to_tile_center(world_pos: Vector2) -> Vector2:
+	var cell := world_to_cell(world_pos)
+	return cell_to_world(cell)
