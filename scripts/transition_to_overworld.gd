329990dc5e6a2581047e090bfe4269f44dc47ddf
Aya <Sherbnya@gmail.com>
2026-04-05 01:42:42 +0100

    Add configurable overworld transition targets

--- scripts/transition_to_overworld.gd
+++ scripts/transition_to_overworld.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@export var node_to_transition_to: String
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("sidescroll-player"):
-		Scenemanager.change_scene_to_overworld("TransitionToNorthernPalace")
+		Scenemanager.change_scene_to_overworld(node_to_transition_to)
 		
