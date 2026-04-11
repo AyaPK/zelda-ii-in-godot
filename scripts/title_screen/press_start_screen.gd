8dfc50ba8f5a64593f7f76c639387b2987aef12f
Aya <Sherbnya@gmail.com>
2026-04-11 21:58:10 +0100

    Add character selection screen

--- scripts/title_screen/press_start_screen.gd
+++ scripts/title_screen/press_start_screen.gd
@@ -14,3 +14,7 @@ func _on_story_anim_animation_finished(anim_name: StringName) -> void:
 	$TextStartScroll.wait_time = 34
 	$TitleAnim.play("scroll")
 	$TextStartScroll.start()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("attack") or Input.is_action_just_pressed("start"):
+		get_tree().change_scene_to_file("res://scenes/title_screen/character_select.tscn")
